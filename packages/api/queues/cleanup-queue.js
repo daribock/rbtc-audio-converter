ee0cb6415f304ae422a4877678872518b7cd3ba0
Darius Kletter <darikletter@gmail.com>
2026-06-06 14:05:47 +0200

    Remove frontend package and all related source files, including components, types, and configuration.

--- packages/api/queues/cleanup-queue.js
+++
@@ -1,9 +0,0 @@
-import { Queue } from "bullmq"
-import { REDIS_CONNECTION_CONFIG } from "../config/config.js"
-import { cleanupWorker } from "../worker.js"
-
-export const cleanupQueue = new Queue("cleanupQueue", {
-  connection: REDIS_CONNECTION_CONFIG,
-})
-
-cleanupWorker()
--- packages/api/queues/create-zip-folder-queue.js
+++
@@ -1,9 +0,0 @@
-import { Queue } from "bullmq"
-import { REDIS_CONNECTION_CONFIG } from "../config/config.js"
-import { createZipFolderWorker } from "../worker.js"
-
-export const createZipFolderQueue = new Queue("createZipFolderQueue", {
-  connection: REDIS_CONNECTION_CONFIG,
-})
-
-createZipFolderWorker()
