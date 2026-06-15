7bfe786a12b4e1f51c5ad8aab12bc1f7075a1467
Darius Kletter <darikletter@gmail.com>
2026-06-15 12:03:10 +0200

    Remove frontend and api package and all related components, utilities, and configurations

--- packages/api/routes/frontend-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import path from "path"
-import { PUBLIC_DIR, ROOT_PATH, ROUTES } from "../config/config.js"
-
-const router = express.Router()
-
-router.use(ROUTES.root, express.static(path.join(ROOT_PATH, PUBLIC_DIR)))
-
-export default router
