62d04cd15482ceb293377c883499e6854b4a4192
Darius Kletter <darikletter@gmail.com>
2026-06-15 14:34:55 +0200

    feat: update webpack renderer rules for improved loader handling

--- packages/electron/webpack.renderer.config.ts
+++ packages/electron/webpack.renderer.config.ts
@@ -3,14 +3,35 @@ import type { Configuration } from 'webpack';
 import { rules } from './webpack.rules';
 import { plugins } from './webpack.plugins';
 
-rules.push({
+const rendererRules = rules.filter((rule) => {
+  if (typeof rule !== 'object' || !rule) {
+    return true;
+  }
+
+  if (rule.use === 'node-loader') {
+    return false;
+  }
+
+  if (
+    typeof rule.use === 'object' &&
+    rule.use !== null &&
+    'loader' in rule.use &&
+    rule.use.loader === '@vercel/webpack-asset-relocator-loader'
+  ) {
+    return false;
+  }
+
+  return true;
+});
+
+rendererRules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 });
 
 export const rendererConfig: Configuration = {
   module: {
-    rules,
+    rules: rendererRules,
   },
   plugins,
   resolve: {
