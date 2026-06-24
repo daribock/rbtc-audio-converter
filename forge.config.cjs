779a82ab44cfa43d2b6d8497160633dfacb1b276
Darius Kletter <darikletter@gmail.com>
2026-06-24 14:20:39 +0200

    feat: update publish workflow and forge configuration for RBTC Audio Converter

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,15 +1,44 @@
 const { FusesPlugin } = require("@electron-forge/plugin-fuses");
 const { FuseV1Options, FuseVersion } = require("@electron/fuses");
 
+const commonLinuxConfig = {
+  categories: ["Audio converter", "Utility"],
+  // icon: {
+  //   "1024x1024": path.resolve(iconDir, "fiddle.png"),
+  //   scalable: path.resolve(iconDir, "fiddle.svg"),
+  // },
+  // mimeType: ["x-scheme-handler/electron-fiddle"],
+};
+
 module.exports = {
   packagerConfig: {
+    name: "RBTC Audio Converter",
+    executableName: "rbtc-audio-converter",
     asar: true,
+    win32metadata: {
+      CompanyName: "DK Technologies",
+      OriginalFilename: "RBTC Audio Converter",
+    },
   },
   rebuildConfig: {},
   makers: [
     {
       name: "@electron-forge/maker-squirrel",
-      config: {},
+      platforms: ["win32"],
+      config: (arch) => ({
+        name: "rbtc-audio-converter",
+        authors: "DK Technologies",
+        exe: "rbtc-audio-converter.exe",
+        // iconUrl:
+        //   "https://raw.githubusercontent.com/darikletter/rbtc-audio-converter/master/assets/icons/rbtc-audio-converter.ico",
+        // loadingGif: "./assets/loading.gif",
+        // noMsi: true,
+        // setupExe: `rbtc-audio-converter-${version}-win32-${arch}-setup.exe`,
+        // setupIcon: path.resolve(iconDir, "rbtc-audio-converter.ico"),
+        // signWithParams: process.env.CERT_FINGERPRINT
+        //   ? `/sha1 ${process.env.CERT_FINGERPRINT} /tr http://timestamp.digicert.com /td SHA256 /fd SHA256`
+        //   : undefined,
+      }),
     },
     {
       name: "@electron-forge/maker-zip",
@@ -17,11 +46,13 @@ module.exports = {
     },
     {
       name: "@electron-forge/maker-deb",
-      config: {},
+      platforms: ["linux"],
+      config: commonLinuxConfig,
     },
     {
       name: "@electron-forge/maker-rpm",
-      config: {},
+      platforms: ["linux"],
+      config: commonLinuxConfig,
     },
   ],
   publishers: [
